d9676e59cfc57e993184a645d211652e6c285469
tiyagallegos <60327779+tiyagallegos@users.noreply.github.com>
2020-03-31 00:28:57 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+console.log("Up and running!");
+var cards = [
+   {
+       rank: "queen",
+       suit: "hearts",
+       cardImage: "images/queen-of-hearts.png",
+   },
+   {
+      rank: "queen",
+      suit: "diamonds",
+      cardImage: "images/queen-of-diamonds.png",
+   },
+
+   {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png",
+   },
+    {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png",
+   },
+];
+
+var cardsInPlay = [];
+
+ function checkForMatch() {
+ 
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+          alert("You found a match!");
+       
+      } else {
+          alert("Sorry, try again");
+        };
+
+  };
+
+function flipCard() {
+    var cardId = this.getAttribute('data-id');
+ 
+  cardsInPlay.push(cards[cardId].rank);
+ this.setAttribute('src', cards[cardId].cardImage);
+
+ if (cardsInPlay.length === 2){
+    checkForMatch();
+ 
+
+ };
+};
+
+console.log(cardsInPlay);
+
+
+ function createBoard() {
+  const cardTable = document.getElementById('game-board');
+  for (var i = 0; i < cards.length; i++) {
+  var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    cardTable.appendChild(cardElement);
+
+  }
+};
+
+createBoard();
